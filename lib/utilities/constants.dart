4ff2069fc2f2bafee7b425257b18523af2203468
Sarbjyotsngh <chahalsarbjyotsingh@gmail.com>
2019-12-31 03:35:48 +0530

    Gradient Background and App Icon and Splash Screen Andriod only

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -4,19 +4,49 @@ const kGradientBackground = BoxDecoration(
   gradient: LinearGradient(
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter,
-    colors: kSleetDayGradient,
+    colors: kClearNightGradient,
   ),
 );
 const kClearNightGradient = [
   Color(0xFF090926),
   Color(0xFF8F5E7D),
 ];
-const kClearDayGradient = [
-  Color(0xFF1F6AB5),
-  Color(0xFFb3e5fc),
+const kClearSkyGradient = [
+  Color(0xFF29b6f6),
+  Color(0xFF29b6f6),
 ];
 
 const kSleetDayGradient = [
   Color(0xFF303030),
   Color(0xFFbdbdbd),
+];
+
+const kHotDayGradient = [
+  Color(0xFF5A0418),
+  Color(0xFFEA5A14),
+];
+
+const kStormGradient = [
+  Color(0xFF252A5C),
+  Color(0xFF976562),
+];
+
+const kSandStormGradient = [
+  Color(0xFF8A390E),
+  Color(0xFFD89045),
+];
+
+const kTornadoGradient = [
+  Color(0xFF2b1e18),
+  Color(0xFF795548),
+];
+
+const kFogGradient = [
+  Color(0xFF976562),
+  Color(0xFF252A5C),
+];
+
+const kCloudyGradient = [
+  Color(0xFF1F6AB5),
+  Color(0xFFb3e5fc),
 ];
